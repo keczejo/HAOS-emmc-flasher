7767f7bec985f44118aa43a8e0d9bddcc05d5e6a
keczejo <61348735+keczejo@users.noreply.github.com>
2022-04-05 22:15:20 +0200

    Update haos.sh

--- haos.sh
+++ haos.sh
@@ -3,7 +3,7 @@ cd /tmp
 echo "Downloading OS"
 wget https://github.com/home-assistant/operating-system/releases/download/7.6/haos_generic-x86-64-7.6.img.xz -O haos.img.xz
 echo "Flashing OS"
-xzcat haos.img.xz | dd of=/dev/mmcblk1 status=progress
+xzcat haos.img.xz | dd of=/dev/mmcblk0 status=progress
 echo "Done"
 sleep 20
 reboot
